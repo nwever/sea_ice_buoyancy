d0384c972eb4936f2cdccdfca7e073bdc0661b23
Nander Wever <nander.wever@slf.ch>
2025-08-04 17:53:40 +0200

    Naming consistency

--- simulations/run_snowpack.sh
+++ simulations/run_snowpack.sh
@@ -1,5 +1,5 @@
-fullpath=$(which snowpack)
-if [[ -z "${fullpath}" ]]; then
+fullpath_snowpack=$(which snowpack)
+if [[ -z "${fullpath_snowpack}" ]]; then
 	echo "ERROR: cannot find snowpack binary. Make sure \$PATH is set correctly to contain a path to the snowpack binary." >&2
 	exit 1
 fi
@@ -20,7 +20,7 @@ do
 				echo "IMPORT_AFTER  = ../io_test_advanced_buoyancy.ini" >> snowpack_sim_${floe}/io_PS81-${inp}${floe}${suffix}.ini
 				mkdir -p snowpack_sim_${floe}/output_advanced_buoyancy_model/
 			fi
-			echo "pushd snowpack_sim_${floe}; ${fullpath} -c io_PS81-${inp}${floe}${suffix}.ini -e NOW > ${inp}${floe}${suffix}.log 2>&1; popd" >> to_run.lst
+			echo "pushd snowpack_sim_${floe}; ${fullpath_snowpack} -c io_PS81-${inp}${floe}${suffix}.ini -e NOW > ${inp}${floe}${suffix}.log 2>&1; popd" >> to_run.lst
 		done
 	done
 done
